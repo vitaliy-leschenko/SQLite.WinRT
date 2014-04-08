6966ee2369695d303b9b6b4e3158ddaa5ad50a2a
Виталий Лещенко <v.leschenko@vitaliy.org>
2014-04-08 22:57:19 +0300

    NuGet

---
+++ NuGetPackage/tools/netcore45/install.ps1
@@ -0,0 +1,4 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host "Successfully added a reference to the extension SDK SQLite for Windows Runtime."
+Write-Host "Please, verify that the extension SDK SQLite for Windows Runtime, from the SQLite.org site (http://www.sqlite.org/2014/sqlite-winrt-*.vsix), has been properly installed."
---
+++ NuGetPackage/tools/netcore451/install.ps1
@@ -0,0 +1,4 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host "Successfully added a reference to the extension SDK SQLite for Windows Runtime (Windows 8.1)."
+Write-Host "Please, verify that the extension SDK SQLite for Windows Runtime (Windows 8.1), from the SQLite.org site (http://www.sqlite.org/2014/sqlite-winrt81-*.vsix), has been properly installed."
