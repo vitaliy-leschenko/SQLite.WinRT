687b724d5b13ce5888b374043ca59818b48385f6
Виталий Лещенко <v.leschenko@vitaliy.org>
2014-04-08 18:03:22 +0300

    Extend API

---
+++ SQLite.WinRT/TableQuery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SQLite.WinRT
+{
+    public partial class TableQuery<T>
+    {
+        public TColumn Min<TColumn>(Expression<Func<T, TColumn>> column)
+        {
+            var map = Connection.GetMapping(typeof(T));
+            var expression = column.Body as MemberExpression;
+            if (expression != null)
+            {
+                var name = expression.Member.Name;
+                var c = map.Columns.First(t => t.PropertyName == name);
+
+                return GenerateCommand("min(\"" + c.Name + "\")").ExecuteScalar<TColumn>();
+            }
+            return default(TColumn);
+        }
+
+        public TColumn Max<TColumn>(Expression<Func<T, TColumn>> column)
+        {
+            var expression = column.Body as MemberExpression;
+            if (expression != null)
+            {
+                var name = expression.Member.Name;
+                var map = Connection.GetMapping(typeof(T));
+                var c = map.Columns.First(t => t.PropertyName == name);
+
+                return GenerateCommand("max(\"" + c.Name + "\")").ExecuteScalar<TColumn>();
+            }
+            return default(TColumn);
+        }
+    }
+}
