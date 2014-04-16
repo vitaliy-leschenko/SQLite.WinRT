b3564455d841eced55bdf14c0d359f28f8e81adf
Виталий Лещенко <v.leschenko@vitaliy.org>
2014-04-16 19:01:53 +0300

    hotfix

--- buildnuget.cmd
+++ buildnuget.cmd
@@ -1,8 +1,8 @@
 set msbuild=C:\Program Files (x86)\MSBuild\12.0\bin
 
-"%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x86 /t:Rebuild
-"%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x64 /t:Rebuild
-"%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=ARM /t:Rebuild
+rem "%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x86 /t:Rebuild
+rem "%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x64 /t:Rebuild
+rem "%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=ARM /t:Rebuild
 
 xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.dll     NuGetPackage\lib\netcore45
 xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\netcore45
@@ -13,6 +13,10 @@ xcopy /y SQLite.WinRT\bin\Release\SQLite.WinRT.dll NuGetPackage\lib\portable-wp8
 xcopy /y SQLite.WinRT.WindowsPhone8\bin\ARM\Release\SQLite.WinRT.dll     NuGetPackage\lib\wp8
 xcopy /y SQLite.WinRT.WindowsPhone8\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\wp8
 xcopy /y SQLite.WinRT.WindowsPhone8\bin\ARM\Release\Sqlite.winmd         NuGetPackage\lib\wp8
+
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\ARM\Release\SQLite.WinRT.dll     NuGetPackage\lib\wpa81
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\wpa81
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\ARM\Release\Sqlite.winmd         NuGetPackage\lib\wpa81
 
 xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.dll     NuGetPackage\build\netcore45\ARM
 xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\build\netcore45\ARM
@@ -35,5 +39,16 @@ xcopy /y SQLite.WinRT.WindowsPhone8\bin\x86\Release\SQLite.WinRT.dll     NuGetPa
 xcopy /y SQLite.WinRT.WindowsPhone8\bin\x86\Release\SQLite.WinRT.Ext.dll NuGetPackage\build\wp8\x86
 xcopy /y Sqlite\bin\Win32\Release\Sqlite.winmd                           NuGetPackage\build\wp8\x86
 xcopy /y Sqlite\bin\Win32\Release\Sqlite.dll                             NuGetPackage\build\wp8\x86
+
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\ARM\Release\SQLite.WinRT.dll     NuGetPackage\build\wpa81\ARM
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\build\wpa81\ARM
+xcopy /y SQLite.WinRT.WindowsPhone81.Proxy\bin\ARM\Release\Sqlite.winmd   NuGetPackage\build\wpa81\ARM
+xcopy /y SQLite.WinRT.WindowsPhone81.Proxy\bin\ARM\Release\SQLite.WinRT.WindowsPhone81.Proxy.dll     NuGetPackage\build\wpa81\ARM
+
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\x86\Release\SQLite.WinRT.dll     NuGetPackage\build\wpa81\x86
+xcopy /y SQLite.WinRT.WindowsPhone81\bin\x86\Release\SQLite.WinRT.Ext.dll NuGetPackage\build\wpa81\x86
+xcopy /y SQLite.WinRT.WindowsPhone81.Proxy\bin\Win32\Release\Sqlite.winmd NuGetPackage\build\wpa81\x86
+xcopy /y SQLite.WinRT.WindowsPhone81.Proxy\bin\Win32\Release\SQLite.WinRT.WindowsPhone81.Proxy.dll   NuGetPackage\build\wpa81\x86
+
 
 .nuget\NuGet.exe pack NuGetPackage\Package.nuspec
