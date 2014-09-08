899ff94a6a5bde6fe88c952b90053ee4f8965165
Виталий Лещенко <v.leschenko@vitaliy.org>
2014-09-08 19:01:16 +0300

    Build 3.8.6.1

--- buildnuget.cmd
+++ buildnuget.cmd
@@ -1,13 +1,15 @@
-xcopy /y SQLite.WinRT\bin\Release\SQLite.WinRT.dll NuGetPackage\lib\portable-net45+win+wpa81+wp8
+xcopy /y SQLite.WinRT\bin\Release\SQLite.WinRT.dll NuGetPackage\lib\portable-net45+win8+wpa81+wp8
 
-xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\netcore45-hidden\ARM
-xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\netcore45-hidden\ARM
+xcopy /y SQLite.WinRT.net45\bin\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\net45-hidden
 
-xcopy /y SQLite.WinRT.Windows81\bin\x64\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\netcore45-hidden\x64
-xcopy /y SQLite.WinRT.Windows81\bin\x64\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\netcore45-hidden\x64
+xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\win81-hidden\ARM
+xcopy /y SQLite.WinRT.Windows81\bin\ARM\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\win81-hidden\ARM
 
-xcopy /y SQLite.WinRT.Windows81\bin\x86\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\netcore45-hidden\x86
-xcopy /y SQLite.WinRT.Windows81\bin\x86\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\netcore45-hidden\x86
+xcopy /y SQLite.WinRT.Windows81\bin\x64\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\win81-hidden\x64
+xcopy /y SQLite.WinRT.Windows81\bin\x64\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\win81-hidden\x64
+
+xcopy /y SQLite.WinRT.Windows81\bin\x86\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\win81-hidden\x86
+xcopy /y SQLite.WinRT.Windows81\bin\x86\Release\SQLite.WinRT.Ext.pri NuGetPackage\lib\win81-hidden\x86
 
 xcopy /y SQLite.WinRT.WindowsPhone8\bin\ARM\Release\SQLite.WinRT.Ext.dll NuGetPackage\lib\wp8-hidden\ARM
 xcopy /y Sqlite.WP80\bin\ARM\Release\Sqlite.WP80.winmd                   NuGetPackage\lib\wp8-hidden\ARM
