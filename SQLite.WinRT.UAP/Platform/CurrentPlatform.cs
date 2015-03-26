7a509abaf3d198362232f6aaba441ad51284c65f
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-03-26 20:39:06 +0300

    UAP

---
+++ SQLite.WinRT.UAP/Platform/CurrentPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SQLite.WinRT
+{
+    public class CurrentPlatform: IPlatform
+    {
+        public ISQLiteProvider SQLiteProvider
+        {
+            get { return WinRT.SQLiteProvider.Instance; }
+        }
+
+        public IPlatformStorage PlatformStorage
+        {
+            get { return WinRT.PlatformStorage.Instance; }
+        }
+    }
+}
