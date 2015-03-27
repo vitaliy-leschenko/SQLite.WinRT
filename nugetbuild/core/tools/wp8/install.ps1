a486107b517ed54325629ff843062bfdeed6b872
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-03-27 14:36:35 +0300

    create nuget packages for UAP

---
+++ nugetbuild/core/tools/wp8/install.ps1
@@ -0,0 +1,6 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host
+Write-Host "Please make sure that target platform is set to x86, x64 or ARM."
+Write-Host "Add reference to SDK:"
+Write-Host "- SQLite for Windows Phone"
---
+++ nugetbuild/core/tools/wpa81/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host
+Write-Host "Please make sure that target platform is set to x86, x64 or ARM."
+Write-Host "Add reference to SDK:"
+Write-Host "- SQLite for Windows Phone 8.1"
+Write-Host "- Microsoft Visual C++ 2013 Runtime Package for Windows Phone"
