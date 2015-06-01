df57f3ee33302bdf9be1c7866f2139a7385f6175
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-06-01 13:42:01 +0300

    Fix build on Windows 10

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-set msbuild=C:\Program Files (x86)\MSBuild\12.0\bin
+set msbuild=C:\Program Files (x86)\MSBuild\14.0\bin
 
 "%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x86 /t:Rebuild
 "%msbuild%\msbuild.exe" /p:Configuration=Release /p:Platform=x64 /t:Rebuild
