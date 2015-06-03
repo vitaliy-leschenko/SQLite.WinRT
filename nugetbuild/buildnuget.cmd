8eea603d6834828ea6a3dd6ff5f859c49e91f3f1
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-06-03 20:48:59 +0300

    Add dependency to SQLite.Core

---
+++ nugetbuild/buildnuget.cmd
@@ -0,0 +1,4 @@
+NuGet.exe pack SQLite.WinRT.nuspec
+NuGet.exe pack SQLite.WinRT.UAP.nuspec
+NuGet.exe pack SQLite.WinRT.win81.nuspec
+NuGet.exe pack SQLite.WinRT.wpa81.nuspec
--- nugetbuild/tools/uap/install.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host
-Write-Host "Please make sure that target platform is set to x86, x64 or ARM."
-Write-Host "Add reference to SDK:"
-Write-Host "- Microsoft Visual C++ 14 AppLocal Runtime Package for Windows UAP"
