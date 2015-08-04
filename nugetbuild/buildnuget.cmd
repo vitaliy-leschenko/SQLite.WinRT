4eb795116207987270f36735d2978b189bcc0a76
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-04 10:58:51 +0300

    nuget

--- nugetbuild/buildnuget.cmd
+++ nugetbuild/buildnuget.cmd
@@ -1,4 +1,7 @@
 NuGet.exe pack SQLite.WinRT.nuspec
 NuGet.exe pack SQLite.WinRT.UAP.nuspec
+NuGet.exe pack SQLite.WinRT.UAP-arm.nuspec
+NuGet.exe pack SQLite.WinRT.UAP-x64.nuspec
+NuGet.exe pack SQLite.WinRT.UAP-x86.nuspec
 NuGet.exe pack SQLite.WinRT.win81.nuspec
 NuGet.exe pack SQLite.WinRT.wpa81.nuspec
