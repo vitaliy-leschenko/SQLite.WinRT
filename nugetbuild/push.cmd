c47785363cd666f82f3caab675f18ce223d2c983
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-05 01:59:00 +0300

    nuget

---
+++ nugetbuild/push.cmd
@@ -0,0 +1,4 @@
+NuGet.exe push SQLite.WinRT.UAP-arm.3.8.11.152.nupkg
+NuGet.exe push SQLite.WinRT.UAP-x64.3.8.11.152.nupkg
+NuGet.exe push SQLite.WinRT.UAP-x86.3.8.11.152.nupkg
+NuGet.exe push SQLite.WinRT.UAP.3.8.11.152.nupkg
