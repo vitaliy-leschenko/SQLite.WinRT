ae6db71db634e0c94c3629397686b05b4d7d13f8
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-06 00:59:45 +0300

    Create NuGet package for UAP

--- nugetbuild/buildnuget.cmd
+++ nugetbuild/buildnuget.cmd
@@ -1,7 +1,4 @@
 NuGet.exe pack SQLite.WinRT.nuspec
 NuGet.exe pack SQLite.WinRT.UAP.nuspec
-NuGet.exe pack SQLite.WinRT.UAP-arm.nuspec
-NuGet.exe pack SQLite.WinRT.UAP-x64.nuspec
-NuGet.exe pack SQLite.WinRT.UAP-x86.nuspec
 NuGet.exe pack SQLite.WinRT.win81.nuspec
 NuGet.exe pack SQLite.WinRT.wpa81.nuspec
