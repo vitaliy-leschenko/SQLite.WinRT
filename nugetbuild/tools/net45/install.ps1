b7f61b3a7dfae156a11b4817ac009750c5cd2411
Виталий Лещенко <v.leschenko@vitaliy.org>
2016-04-12 23:48:16 +0300

    Merge SQLite.Core into SQLite.WinRT

--- nugetbuild/tools/net45/install.ps1
+++
@@ -1,8 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host
-Write-Host "Please make sure that:"
-Write-Host "- you call global::NativeDllHelper.SetDllDirectory() at startup." -foregroundcolor Blue
-Write-Host "- property 'Copy to Output Directory' of x86\sqlite3.dll is setted to 'Copy if newer'." -foregroundcolor Blue
-Write-Host "- property 'Copy to Output Directory' of x64\sqlite3.dll is setted to 'Copy if newer'." -foregroundcolor Blue
-
